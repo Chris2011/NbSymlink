ae09bf536f0a376fbf26974e9397d695ef990142
Chris2011 <Christian.Lenz@gmx.net>
2015-07-14 19:08:21 +0200

    Added project files and first action to add an action to any folder.

---
+++ src/org/chrisle/netbeans/plugins/nbsymlink/NbSymlinkAction.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.chrisle.netbeans.plugins.nbsymlink;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import org.openide.loaders.DataObject;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "Tools",
+        id = "org.chrisle.netbeans.plugins.nbsymlink.NbSymlinkAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_NbSymlinkAction"
+)
+
+@ActionReferences({
+    @ActionReference(path = "Loaders/folder/any/Actions", position = 0),
+})
+@Messages("CTL_NbSymlinkAction=Create Symlink")
+public final class NbSymlinkAction implements ActionListener {
+
+    private final DataObject context;
+
+    public NbSymlinkAction(DataObject context) {
+        this.context = context;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+        JOptionPane.showMessageDialog(null, context.toString());
+    }
+}
