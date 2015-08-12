1d4687c2c8723c1f1c589c177ffb01a5927754af
Chris2011 <Christian.Lenz@gmx.net>
2015-08-12 21:48:50 +0200

    Register the action for projects and folders and add call the subactions.

--- src/org/chrisle/netbeans/plugins/nbsymlink/NbSymlinkAction.java
+++ src/org/chrisle/netbeans/plugins/nbsymlink/NbSymlinkAction.java
@@ -1,37 +1,48 @@
 package org.chrisle.netbeans.plugins.nbsymlink;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
-import org.openide.loaders.DataObject;
+import javax.swing.AbstractAction;
+import javax.swing.JMenu;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle;
 import org.openide.util.NbBundle.Messages;
+import org.openide.util.actions.Presenter;
+import javax.swing.JMenuItem;
 
 @ActionID(
         category = "Tools",
         id = "org.chrisle.netbeans.plugins.nbsymlink.NbSymlinkAction"
 )
 @ActionRegistration(
-        displayName = "#CTL_NbSymlinkAction"
+    displayName = "#CTL_NbSymlinkAction"
 )
 
 @ActionReferences({
-    @ActionReference(path = "Loaders/folder/any/Actions", position = 0),
+    @ActionReference(path = "Loaders/folder/any/Actions", position = 100),
+    @ActionReference(path = "Projects/Actions", position = 100)
 })
 @Messages("CTL_NbSymlinkAction=Create Symlink")
-public final class NbSymlinkAction implements ActionListener {
+public final class NbSymlinkAction extends AbstractAction implements Presenter.Popup {
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // this = submenu => do nothing
+    }
 
-    private final DataObject context;
-
-    public NbSymlinkAction(DataObject context) {
-        this.context = context;
+    public NbSymlinkAction() {
+        super(NbBundle.getMessage(NbSymlinkAction.class, "CTL_NbSymlinkAction"));
     }
 
     @Override
-    public void actionPerformed(ActionEvent ev) {
-        JOptionPane.showMessageDialog(null, context.toString());
+    public JMenuItem getPopupPresenter() {
+        JMenu menu = new JMenu(this);
+
+        menu.add(new SourceAction());
+        menu.add(new TargetAction());
+
+        return menu;
     }
 }
