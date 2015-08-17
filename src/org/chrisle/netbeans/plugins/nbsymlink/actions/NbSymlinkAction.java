77de4e45bcaaf3f78b5c8e39e274f7d16e8a2bff
Chris <chrl@spreadshirt.net>
2015-08-17 17:22:00 +0200

    Refactored code inside the NbSymlinkAction.

--- src/org/chrisle/netbeans/plugins/nbsymlink/actions/NbSymlinkAction.java
+++ src/org/chrisle/netbeans/plugins/nbsymlink/actions/NbSymlinkAction.java
@@ -1,6 +1,7 @@
 package org.chrisle.netbeans.plugins.nbsymlink.actions;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
 import javax.swing.AbstractAction;
 import javax.swing.JMenu;
 import org.openide.awt.ActionID;
@@ -11,8 +12,12 @@ import org.openide.util.NbBundle;
 import org.openide.util.NbBundle.Messages;
 import org.openide.util.actions.Presenter;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import org.chrisle.netbeans.plugins.nbsymlink.subactions.SourceAction;
 import org.chrisle.netbeans.plugins.nbsymlink.subactions.TargetAction;
+import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataObject;
+import org.openide.windows.TopComponent;
 
 @ActionID(
         category = "Tools",
@@ -38,10 +43,16 @@ public final class NbSymlinkAction extends AbstractAction implements Presenter.P
     
     @Override
     public JMenuItem getPopupPresenter() {
+        TopComponent activeTC = TopComponent.getRegistry().getActivated();
+        DataObject dataLookup = activeTC.getLookup().lookup(DataObject.class);
+        File primaryFile = FileUtil.toFile(dataLookup.getPrimaryFile());
+        
+        this.setEnabled(primaryFile != null);
+        
         JMenu menu = new JMenu(this);
 
-        menu.add(new SourceAction());
-        menu.add(new TargetAction());
+        menu.add(new SourceAction(primaryFile));
+        menu.add(new TargetAction(primaryFile));
 
         return menu;
     }
